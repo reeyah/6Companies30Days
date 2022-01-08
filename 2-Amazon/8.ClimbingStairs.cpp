b20536487666a9a266333efdf36c9423d360e178
Reeya <reeyahazarika@gmail.com>
2022-01-08 11:09:08 +0530

    C2Q8 created

---
+++ 2-Amazon/8.ClimbingStairs.cpp
@@ -0,0 +1,4 @@
+// Count ways to N'th Stair(Order does not matter)
+// https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/
+// 70. Climbing Stairs
+// https://leetcode.com/problems/climbing-stairs/
