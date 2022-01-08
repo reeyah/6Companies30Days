81ebb5319cb5dd7019267e51debbbbe117c38d2a
Reeya <reeyahazarika@gmail.com>
2022-01-08 11:26:52 +0530

    C2Q8 added

--- 2-Amazon/8.ClimbingStairs.cpp
+++ 2-Amazon/8.ClimbingStairs.cpp
@@ -1,4 +1,32 @@
 // Count ways to N'th Stair(Order does not matter)
 // https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1/
-// 70. Climbing Stairs
-// https://leetcode.com/problems/climbing-stairs/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    public:
+    long long countWays(int m)
+    {
+        // your code here
+        return m/2 + 1; 
+    }
+};
+
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+} 
